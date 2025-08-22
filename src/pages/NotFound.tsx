208154040b194cfc99a286081f47c5974c6f900b
carol barroso <carolbarrosowork@gmail.com>
2025-08-22 02:53:46 +0000

    ajeitado

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -17,7 +17,7 @@ const NotFound = () => {
         <h1 className="text-4xl font-bold mb-4">404</h1>
         <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
         <a href="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
+          Voltar
         </a>
       </div>
     </div>
